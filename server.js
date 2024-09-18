5b35a6c49ed6522a655b48f09063d0a58eb41a6c
JM <Not given>
2024-09-18 10:23:18 -0400

    Created the Hello World server.js program

---
+++ server.js
@@ -0,0 +1,22 @@
+/*
+The express library handles server communication 
+and hosts to http://localhost:3000/
+
+https://expressjs.com/en/starter/hello-world.html
+
+Start the server by running 'node server' 
+in the terminal after installing Node/NPM. Then open
+http://localhost:3000/ on your browser.
+*/
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', function(req,res){
+  res.send('Hello World!')
+})
+
+app.listen(port, function(){
+  console.log(`Example app listening on port ${port}`)
+})
