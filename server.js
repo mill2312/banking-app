a35fb6a7c09de357b14e86a251b9fc2fe0f491a5
mill2312 <mill2312@miamioh.edu>
2024-09-20 14:24:57 +0000

    Added example code for hosting pages with documentation

--- server.js
+++ server.js
@@ -1,22 +1,61 @@
 /*
-The express library handles server communication 
-and hosts to http://localhost:3000/
+  The express library handles server communication 
+  and hosts to http://localhost:3000/
 
-https://expressjs.com/en/starter/hello-world.html
+  https://expressjs.com/en/starter/hello-world.html
 
-Start the server by running 'node server' 
-in the terminal after installing Node/NPM. Then open
-http://localhost:3000/ on your browser.
+  Start the server by running 'node server' 
+  in the terminal after installing Node/NPM. Then open
+  http://localhost:3000/ on your browser.
+
+  ---------------------------------------------------------
+
+  app.listen() starts the server and has a callback argument
+  where we display a message.
+
+  To serve requests to certain links, we use app.get(link, callback).
+  The link is everything after www.google.com. 
+  For example www.google.com/search, the link would be /search.
+
+  After the request is made to this link, the callback function is called
+  with a request (req) and response (res). The request lets us interact
+  with the clients web request. The response lets us interact with what
+  we will send back.
+
+  We use the response arameter's send function (e.g. res.send())
+  to send a text response to the client from this server, 
+  which is interpreted as HTML.
 */
 
-const express = require('express')
+const fs = require("fs") // File System (allows us to read files)
+const express = require('express') // Express local server-hosting library
 const app = express()
 const port = 3000
 
-app.get('/', function(req,res){
-  res.send('Hello World!!!test')
+// Starts the server.
+app.listen(port, function(){
+  console.log("Example app listening on port " + port)
 })
 
-app.listen(port, function(){
-  console.log(`Example app listening on port ${port}`)
+app.get("/", function(req,res){
+  /*
+    Sends HTML with link (a href) to login page
+    https://www.w3schools.com/tags/att_a_href.asp
+
+    Accent (`) allows for multiple lines
+  */
+  res.send(`
+    Hello World!!!<br>
+    <a href="/login">Login Page</a>
+  `)
+  res.end() // End response
+})
+
+app.get("/login", function(req,res){
+  /*
+    Here we send a response from the content of the file
+    login-page.html in the websites folder.
+  */
+  res.send(fs.readFileSync("./websites/login-page.html", "utf-8"))
+  res.end() // End response
 })
