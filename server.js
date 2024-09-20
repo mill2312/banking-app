e3e6cf7c285e14de8b117f88f333f0a2bce6d42b
mill2312 <mill2312@miamioh.edu>
2024-09-20 14:50:55 +0000

    More documentation

--- server.js
+++ server.js
@@ -1,30 +1,47 @@
 /*
-  The express library handles server communication 
-  and hosts to http://localhost:3000/
-
   https://expressjs.com/en/starter/hello-world.html
 
   Start the server by running 'node server' 
   in the terminal after installing Node/NPM. Then open
   http://localhost:3000/ on your browser.
 
+  To stop press CTRL-C in terminal.
+
   ---------------------------------------------------------
 
-  app.listen() starts the server and has a callback argument
+
+      app.listen(port, function(){
+        console.log("Example app listening on port " + port)
+      })
+
+  app.listen() starts the server and has a callback function
   where we display a message.
+
+      
+      app.get("/", function(req,res){
+        res.send("Hello!")
+        res.end() // End response
+      })
 
   To serve requests to certain links, we use app.get(link, callback).
   The link is everything after www.google.com. 
   For example www.google.com/search, the link would be /search.
-
+  
   After the request is made to this link, the callback function is called
   with a request (req) and response (res). The request lets us interact
   with the clients web request. The response lets us interact with what
   we will send back.
 
-  We use the response arameter's send function (e.g. res.send())
+
+    1.  res.send("Hello!")
+    2.  res.send("<b>Bold Hello</b>")
+    3.  res.send(fs.readFileSync("./websites/login-page.html", "utf-8"))
+
+  We use the response parameter's send function (e.g. res.send())
   to send a text response to the client from this server, 
-  which is interpreted as HTML.
+  which is interpreted as HTML. We can send content of file using
+  fs.readFileSync("[filelocation]", "utf-8").
+
 */
 
 const fs = require("fs") // File System (allows us to read files)
